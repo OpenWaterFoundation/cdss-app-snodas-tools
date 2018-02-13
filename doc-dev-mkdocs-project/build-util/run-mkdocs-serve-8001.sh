ba8f6b2b3da5a74751c853b8ca3cba160c04ed9b
Steve Malers <steve.malers@openwaterfoundation.org>
2018-02-13 04:30:51 -0700

    Re-add run scripts for documentation - somehow got deleted.

---
+++ doc-dev-mkdocs-project/build-util/run-mkdocs-serve-8001.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#
+# Run 'mkdocs serve' on port 8001
+
+# Make sure that this is being run from the build-util folder
+pwd=`pwd`
+dirname=`basename ${pwd}`
+if [ ! ${dirname} = "build-util" ]
+	then
+	echo "Must run from build-util folder"
+	exit 1
+fi
+
+cd ..
+
+echo "View the website using http://localhost:8001"
+echo "Kill the server with CTRL-C"
+mkdocs serve -a 0.0.0.0:8001
+#mkdocs serve
